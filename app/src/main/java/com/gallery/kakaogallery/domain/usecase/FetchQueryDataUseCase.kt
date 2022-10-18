8bc853ef52c4547e384a9dabeed0010c35dfc3d0
wjchoi96 <wjchoi96@naver.com>
2022-10-18 20:34:22 +0900

    [Modify]#12 FetchSearchDataQueryUseCase
     - UseCase에서 Error를 catch하여 적절한 래퍼클래스로 래핑하여 데이터 리턴

--- app/src/main/java/com/gallery/kakaogallery/domain/usecase/FetchQueryDataUseCase.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/usecase/FetchQueryDataUseCase.kt
@@ -2,9 +2,6 @@ package com.gallery.kakaogallery.domain.usecase
 
 import android.util.Log
 import com.gallery.kakaogallery.domain.model.ImageModel
-import com.gallery.kakaogallery.domain.model.MaxPageException
-import com.gallery.kakaogallery.domain.model.Result
-import com.gallery.kakaogallery.domain.model.ResultError
 import com.gallery.kakaogallery.domain.repository.ImageRepository
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -12,24 +9,17 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class FetchQueryDataUseCase(
     private val imageRepository: ImageRepository
 ) {
-
     operator fun invoke(query: String, page: Int): Observable<Result<List<ImageModel>>>{
         return imageRepository
             .fetchQueryData(query, page)
             .observeOn(Schedulers.computation())
             .map {
-                if(it != null)
-                    Result.Success(it)
-                else
-                    Result.Fail(ResultError.Fail)
+                Result.success(it)
             }
             .onErrorReturn {
                 it.printStackTrace()
                 Log.d("TAG", "error debug at useCase => $it")
-                when(it){
-                    is MaxPageException -> Result.Fail(ResultError.MaxPage)
-                    else -> Result.Fail(ResultError.Crash)
-                }
+                Result.failure(it)
             }
     }
 }
