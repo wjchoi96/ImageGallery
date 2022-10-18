e224445be5ab1a1d30acb520c05c666b407bf29e
wjchoi96 <wjchoi96@naver.com>
2022-10-18 18:41:54 +0900

    [Modify]#12 VideoSearchDataSource 리턴 형식 변경
     - Result를 사용하여 에러처리를 진행하지 않고, 예외를 발생시키는 방식으로 진행
     - 이때 예외를 throw하지 않고 Observable을 통해 전달

--- app/src/main/java/com/gallery/kakaogallery/data/datasource/VideoSearchDataSource.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/VideoSearchDataSource.kt
@@ -1,9 +1,8 @@
 package com.gallery.kakaogallery.data.datasource
 
 import com.gallery.kakaogallery.data.entity.remote.response.VideoSearchResponse
-import com.gallery.kakaogallery.domain.model.Result
 import io.reactivex.rxjava3.core.Observable
 
 interface VideoSearchDataSource {
-    fun fetchVideoQueryRes(query : String, page : Int): Observable<Result<List<VideoSearchResponse.Document>>>
+    fun fetchVideoQueryRes(query : String, page : Int): Observable<List<VideoSearchResponse.Document>>
 }
