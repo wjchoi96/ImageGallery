c65aa4920cc80b1b25406186a20a9dc9bd92b2de
wjchoi96 <wjchoi96@naver.com>
2022-10-20 19:06:20 +0900

    [Feature]#19 Timber Debug Tree 적용

--- app/src/main/java/com/gallery/kakaogallery/presentation/application/KakaoGalleryApplication.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/application/KakaoGalleryApplication.kt
@@ -1,8 +1,10 @@
 package com.gallery.kakaogallery.presentation.application
 
 import android.app.Application
+import com.gallery.kakaogallery.BuildConfig
 import com.gallery.kakaogallery.presentation.network.NetworkUtil
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class KakaoGalleryApplication : Application() {
@@ -22,6 +24,10 @@ class KakaoGalleryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         netWorkUtil = NetworkUtil.instance
         netWorkUtil?.register()
