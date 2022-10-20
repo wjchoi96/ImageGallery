8660d6169f217530862a334baa06f8a4b0f8aab8
wjchoi96 <wjchoi96@naver.com>
2022-10-20 21:56:32 +0900

    [Remove] FragmentRootHandler, BaseFragmentUseHandler
     - Activityd의 HeaderComp를 사용하지 않게 됨으로 인해 제거

--- app/src/main/java/com/gallery/kakaogallery/presentation/ui/root/FragmentRootHandler.kt
+++
@@ -1,7 +0,0 @@
-package com.gallery.kakaogallery.presentation.ui.root
-
-import com.gallery.kakaogallery.presentation.ui.comp.HeaderComp
-
-interface FragmentRootHandler {
-    fun getHeaderCompFromRoot(): HeaderComp?
-}
