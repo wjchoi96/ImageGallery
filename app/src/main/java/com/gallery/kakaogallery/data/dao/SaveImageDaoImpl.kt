5b0c65f4d77a3b182a273ad60d9ff19aeace980e
wjchoi96 <wjchoi96@naver.com>
2022-10-25 01:41:14 +0900

    [Feature]#27 SaveImageDao를 싱글톤으로 설정
     - save image stream을 공유하기 위해 싱글톤으로 설정

--- app/src/main/java/com/gallery/kakaogallery/data/dao/SaveImageDaoImpl.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/dao/SaveImageDaoImpl.kt
@@ -8,7 +8,9 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import timber.log.Timber
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class SaveImageDaoImpl @Inject constructor(
     private val sp: KakaoGallerySharedPreferences
 ) : SaveImageDao {
--- app/src/main/java/com/gallery/kakaogallery/presentation/di/DaoModule.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/di/DaoModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DaoModule {
 
     @Binds
+    @Singleton
     abstract fun bindSaveImageDao(impl: SaveImageDaoImpl): SaveImageDao
 }
