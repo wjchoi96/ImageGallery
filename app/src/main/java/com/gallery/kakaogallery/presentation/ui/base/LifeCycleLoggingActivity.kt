2539ad9780745314b485f8cebd68089fdb8c0ad4
wjchoi96 <wjchoi96@naver.com>
2022-10-31 21:28:05 +0900

    [Add]#28 LifecycleLoggingActivity 추가
    -  생명주기를 관찰해 로그를 찍는 LifecycleLoggingActivity 추가

---
+++ app/src/main/java/com/gallery/kakaogallery/presentation/ui/base/LifeCycleLoggingActivity.kt
@@ -0,0 +1,36 @@
+package com.gallery.kakaogallery.presentation.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gallery.kakaogallery.presentation.ui.LifeCycleLogObserver
+import timber.log.Timber
+
+abstract class LifeCycleLoggingActivity: AppCompatActivity() {
+
+    private val logLifecycleObserver = LifeCycleLogObserver(this::class.java.simpleName)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycle.addObserver(logLifecycleObserver)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Timber.tag(logLifecycleObserver.tag).i("onRestoreInstanceState savedInstanceState[$savedInstanceState]")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Timber.tag(logLifecycleObserver.tag).i("onSaveInstanceState")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.tag(logLifecycleObserver.tag).i("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(logLifecycleObserver)
+    }
+}
