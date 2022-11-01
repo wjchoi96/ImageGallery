72a2f8c60b4699d36c7de7471c5f338b15e2aa75
wjchoi96 <wjchoi96@naver.com>
2022-11-01 22:31:57 +0900

    [Refactor]#44 GalleryViewModel UseCase의 결과를 구독하는 코드 수정
     - Single타입은 Result를 래핑하지 않고 onError로 작업 결과, 예외 발생을 통지받도록 수정

--- app/src/main/java/com/gallery/kakaogallery/presentation/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/viewmodel/GalleryViewModel.kt
@@ -20,13 +20,15 @@ class GalleryViewModel @Inject constructor(
     private val removeSaveImageUseCase: RemoveSaveImageUseCase
 ) : DisposableManageViewModel(), ToolBarViewModel {
 
-    private val fetchImageFailMessage: String = resourceProvider.getString(StringResourceProvider.StringResourceId.FetchFailSaveImage)
+    private val fetchImageFailMessage: String =
+        resourceProvider.getString(StringResourceProvider.StringResourceId.FetchFailSaveImage)
     private val selectImageHashMap = mutableMapOf<String, Int>()
 
     private val _saveImages = MutableLiveData<List<ImageModel>>(emptyList())
     val saveImages: LiveData<List<ImageModel>> = _saveImages
 
-    private val _headerTitle = MutableLiveData(resourceProvider.getString(StringResourceProvider.StringResourceId.MenuGallery))
+    private val _headerTitle =
+        MutableLiveData(resourceProvider.getString(StringResourceProvider.StringResourceId.MenuGallery))
     override val headerTitle: LiveData<String> = _headerTitle
 
     private val _selectMode = MutableLiveData(false)
@@ -45,24 +47,22 @@ class GalleryViewModel @Inject constructor(
         fetchSaveImages()
     }
 
-    fun removeSelectImage(){
+    fun removeSelectImage() {
         _dataLoading.value = true
         removeSaveImageUseCase(selectImageHashMap)
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { res ->
+            .subscribe({
                 _dataLoading.value = false
-                res.onSuccess {
-                    when(it){
-                        true -> {
-                            showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveSuccess))
-                            clickSelectModeEvent() // 삭제라면 선택모드 였을테니, toggle 해주면 선택모드가 해제됨
-                        }
-                        else -> showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveFail))
+                when (it) {
+                    true -> {
+                        showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveSuccess))
+                        clickSelectModeEvent() // 삭제라면 선택모드 였을테니, toggle 해주면 선택모드가 해제됨
                     }
-                }.onFailure {
-                    it.printStackTrace()
-                    showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveFail) + " $it")
+                    else -> showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveFail))
                 }
+            }) {
+                it.printStackTrace()
+                showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.RemoveFail) + " $it")
             }.addTo(compositeDisposable)
     }
 
@@ -76,24 +76,28 @@ class GalleryViewModel @Inject constructor(
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { res ->
                 _dataLoading.value = false
-                if(isRefresh) _refreshLoading.value = false
+                if (isRefresh) _refreshLoading.value = false
                 res.onSuccess {
                     _saveImages.value = it
                 }.onFailure {
                     when (it) {
-                        is MaxPageException -> showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.LastPage))
+                        is MaxPageException -> showToast(
+                            resourceProvider.getString(
+                                StringResourceProvider.StringResourceId.LastPage
+                            )
+                        )
                         else -> showToast("$fetchImageFailMessage\n${it.message}")
                     }
                 }
             }.addTo(compositeDisposable)
     }
 
-    fun refreshGalleryEvent(){
+    fun refreshGalleryEvent() {
         _refreshLoading.value = true
         fetchSaveImages(isRefresh = true)
     }
 
-    fun clickRemoveEvent(){
+    fun clickRemoveEvent() {
         if (selectImageHashMap.isEmpty()) {
             _uiEvent.value =
                 SingleEvent(UiEvent.ShowToast(resourceProvider.getString(StringResourceProvider.StringResourceId.NoneSelectImage)))
@@ -102,11 +106,12 @@ class GalleryViewModel @Inject constructor(
         _uiEvent.value = SingleEvent(UiEvent.PresentRemoveDialog(selectImageHashMap.size))
     }
 
-    fun clickSelectModeEvent(){
-        when(_selectMode.value){
+    fun clickSelectModeEvent() {
+        when (_selectMode.value) {
             true -> {
                 unSelectAllImage()
-                _headerTitle.value = resourceProvider.getString(StringResourceProvider.StringResourceId.MenuGallery)
+                _headerTitle.value =
+                    resourceProvider.getString(StringResourceProvider.StringResourceId.MenuGallery)
             }
             else -> _headerTitle.value = resourceProvider.getString(
                 StringResourceProvider.StringResourceId.SelectState,
@@ -116,9 +121,9 @@ class GalleryViewModel @Inject constructor(
         _selectMode.value = !(_selectMode.value ?: false)
     }
 
-    fun touchImageEvent(image: ImageModel, idx: Int){
+    fun touchImageEvent(image: ImageModel, idx: Int) {
         _uiEvent.value = SingleEvent(UiEvent.KeyboardVisibleEvent(false))
-        when (selectMode.value){
+        when (selectMode.value) {
             true ->
                 setSelectImage(image, idx, !selectImageHashMap.containsKey(image.hash))
             else -> {}
@@ -128,10 +133,10 @@ class GalleryViewModel @Inject constructor(
     private fun unSelectAllImage() {
         val images = saveImages.value?.toMutableList() ?: return
         try {
-            for(idx in selectImageHashMap.values){
+            for (idx in selectImageHashMap.values) {
                 images[idx] = images[idx].copy(isSelect = false)
             }
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
         selectImageHashMap.clear()
@@ -142,25 +147,28 @@ class GalleryViewModel @Inject constructor(
         val images = saveImages.value?.toMutableList() ?: return
         try {
             images[idx] = images[idx].copy(isSelect = select)
-            when (select){
+            when (select) {
                 true -> selectImageHashMap[image.hash] = idx
                 else -> selectImageHashMap.remove(image.hash)
             }
             _saveImages.value = images
-            _headerTitle.value = resourceProvider.getString(StringResourceProvider.StringResourceId.SelectState, selectImageHashMap.size)
-        }catch (e: Exception){
+            _headerTitle.value = resourceProvider.getString(
+                StringResourceProvider.StringResourceId.SelectState,
+                selectImageHashMap.size
+            )
+        } catch (e: Exception) {
             e.printStackTrace()
             showToast(resourceProvider.getString(StringResourceProvider.StringResourceId.SelectFail))
         }
     }
 
-    private fun showToast(message: String){
+    private fun showToast(message: String) {
         _uiEvent.value = SingleEvent(UiEvent.ShowToast(message))
     }
 
     sealed class UiEvent {
         data class ShowToast(val message: String) : UiEvent()
-        data class PresentRemoveDialog(val selectCount: Int): UiEvent()
-        data class KeyboardVisibleEvent(val visible: Boolean): UiEvent()
+        data class PresentRemoveDialog(val selectCount: Int) : UiEvent()
+        data class KeyboardVisibleEvent(val visible: Boolean) : UiEvent()
     }
 }
