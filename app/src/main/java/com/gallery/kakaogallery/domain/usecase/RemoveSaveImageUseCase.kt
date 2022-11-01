cc44006e1e4a1b75fa4cad4e5d89821deb539804
wjchoi96 <wjchoi96@naver.com>
2022-11-01 21:00:31 +0900

    [Refactor]#44 RemoveSaveImageUseCase의 리턴 타입을 Single로 수정
     - ImageRepository의 Remove메소드가 Completeable을 리턴하기에 작업이 완료되면 성공, 에러가 발생하면 실패로 판단하여 Single타입으로 데이터 통지를 한번만 하도록 수정

--- app/src/main/java/com/gallery/kakaogallery/domain/usecase/RemoveSaveImageUseCase.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/usecase/RemoveSaveImageUseCase.kt
@@ -1,16 +1,17 @@
 package com.gallery.kakaogallery.domain.usecase
 
 import com.gallery.kakaogallery.domain.repository.ImageRepository
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 class RemoveSaveImageUseCase(
     private val imageRepository: ImageRepository
 ) {
-    operator fun invoke(selectImageHashMap: MutableMap<String, Int>): Observable<Result<Boolean>> {
+    operator fun invoke(selectImageHashMap: MutableMap<String, Int>): Single<Result<Boolean>> {
         return imageRepository.removeImages(selectImageHashMap.values.toList())
-            .map {
-                Result.success(it)
-            }.onErrorReturn {
+            .toSingle {
+                Result.success(true)
+            }
+            .onErrorReturn {
                 it.printStackTrace()
                 Result.failure(it)
             }
