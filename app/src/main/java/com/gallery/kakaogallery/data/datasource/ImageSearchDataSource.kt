406ca49cfc97439a78298af63ab7449768fda45e
wjchoi96 <wjchoi96@naver.com>
2022-11-01 20:39:38 +0900

    [Refactor]#44 Image Video Search DataSource의 fetch메소드 리턴 타입을 Single로 수정
     - 관련 Api Service의 리턴 타입이 Single이기 때문에, 관련 데이터가 한번만 통지되는것을 의미
     - 때문에 Single로 리턴 타입을 수정

--- app/src/main/java/com/gallery/kakaogallery/data/datasource/ImageSearchDataSource.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/ImageSearchDataSource.kt
@@ -1,8 +1,8 @@
 package com.gallery.kakaogallery.data.datasource
 
 import com.gallery.kakaogallery.data.entity.remote.response.ImageSearchResponse
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 interface ImageSearchDataSource {
-    fun fetchImageQueryRes(query: String, page: Int): Observable<List<ImageSearchResponse.Document>>
+    fun fetchImageQueryRes(query: String, page: Int): Single<List<ImageSearchResponse.Document>>
 }
--- app/src/main/java/com/gallery/kakaogallery/data/datasource/VideoSearchDataSource.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/VideoSearchDataSource.kt
@@ -1,8 +1,8 @@
 package com.gallery.kakaogallery.data.datasource
 
 import com.gallery.kakaogallery.data.entity.remote.response.VideoSearchResponse
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 interface VideoSearchDataSource {
-    fun fetchVideoQueryRes(query: String, page: Int): Observable<List<VideoSearchResponse.Document>>
+    fun fetchVideoQueryRes(query: String, page: Int): Single<List<VideoSearchResponse.Document>>
 }
