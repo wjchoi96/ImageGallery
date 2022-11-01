37a83f556c43c2cb6ee342b444eac3feab2a95ca
wjchoi96 <wjchoi96@naver.com>
2022-11-01 20:38:18 +0900

    [Refactor]#44 Image, Video Search Service의 리턴 타입을 Single로 변경
     - Api Service는 request/response 과정이 한번만 진행되기 때문에 Single로 변경

--- app/src/main/java/com/gallery/kakaogallery/data/service/VideoSearchService.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/service/VideoSearchService.kt
@@ -2,7 +2,7 @@ package com.gallery.kakaogallery.data.service
 
 import com.gallery.kakaogallery.data.constant.ApiAddressConstant
 import com.gallery.kakaogallery.data.entity.remote.response.VideoSearchResponse
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,5 +13,5 @@ interface VideoSearchService {
         @Query("sort") sort: String,
         @Query("page") page: Int, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
         @Query("size") pageSize: Int // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
-    ): Observable<VideoSearchResponse>
+    ): Single<VideoSearchResponse>
 }
