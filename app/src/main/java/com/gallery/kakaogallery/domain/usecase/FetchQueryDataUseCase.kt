309495c4824ed394d19af46fdaee42880c7dbe9c
wjchoi96 <wjchoi96@naver.com>
2022-11-01 22:30:34 +0900

    [Refactor]#44 FetchQueryDataUseCase 리턴 타입을 Result로 래핑하지 않도록 수정
     - Single타입을 리턴하기때문에 onError로 작업 실패, 예외 발생을 통지해도 무방
     - Observable은 onError발생시 스트림이 중단되기에 래퍼클래스로 작업 실패, 예외 발생을 알렸음

--- app/src/main/java/com/gallery/kakaogallery/domain/usecase/FetchQueryDataUseCase.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/usecase/FetchQueryDataUseCase.kt
@@ -8,28 +8,23 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class FetchQueryDataUseCase(
     private val imageRepository: ImageRepository
 ) {
-    operator fun invoke(query: String, page: Int): Single<Result<List<ImageListTypeModel>>> {
+    operator fun invoke(query: String, page: Int): Single<List<ImageListTypeModel>> {
         return if(query.isBlank()){
-            Single.just(Result.success(listOf(ImageListTypeModel.Query(query))))
+            Single.just(listOf(ImageListTypeModel.Query(query)))
         } else {
             imageRepository
                 .fetchQueryData(query, page)
                 .observeOn(Schedulers.computation())
                 .map {
                     when(page){
-                        1 -> Result.success(
-                            listOf(ImageListTypeModel.Query(query)) +
-                                    it.map { image -> ImageListTypeModel.Image(image) }
-                        )
-                        else -> Result.success(
-                            it.map { image -> ImageListTypeModel.Image(image) }
-                        )
+                        1 -> listOf(ImageListTypeModel.Query(query)) +
+                                it.map { image -> ImageListTypeModel.Image(image) }
+                        else -> it.map { image -> ImageListTypeModel.Image(image) }
                     }
                 }
-                .onErrorReturn {
+                .doOnError {
                     it.printStackTrace()
                     println("error debug at useCase => $it")
-                    Result.failure(it)
                 }
         }
     }
