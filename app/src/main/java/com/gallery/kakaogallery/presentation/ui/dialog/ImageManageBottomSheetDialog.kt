0ef8929cd03d03f065ea1081e4b875dd9248e2c1
wjchoi96 <wjchoi96@naver.com>
2022-11-02 22:36:51 +0900

    [Refactor]#48 ImageManageBottomSheetDialog 수정
     - positive, negative listener을 전달받지 않고 callback으로 전달
     - argument로 전달받는 데이터 처리 코드 ViewModel로 이동. 관련 코드 제거
     - factory함수인 get함수의 네이밍을 newInstance로 변경

--- app/src/main/java/com/gallery/kakaogallery/presentation/ui/dialog/ImageManageBottomSheetDialog.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/ui/dialog/ImageManageBottomSheetDialog.kt
@@ -12,48 +12,30 @@ import com.gallery.kakaogallery.R
 import com.gallery.kakaogallery.databinding.DialogImageManageBinding
 import com.gallery.kakaogallery.presentation.viewmodel.ImageManageBottomSheetViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
+@AndroidEntryPoint
 class ImageManageBottomSheetDialog : BottomSheetDialogFragment() {
     companion object {
         private const val TAG = "image_manage_bottom_sheet_dialog"
-        private const val EXTRA_CONTENT = "extra_content"
-        private const val EXTRA_POSITIVE_TEXT = "extra_positive_text"
-        private const val EXTRA_NEGATIVE_TEXT = "extra_negative_text"
 
-        fun get(
+        fun newInstance(
             content: String,
             positiveBtnText: String,
-            negativeBtnText: String,
-            positiveListener: (() -> Unit)? = null,
-            negativeListener: (() -> Unit)? = null
+            negativeBtnText: String
         ) = ImageManageBottomSheetDialog().apply {
             arguments = bundleOf(
-                EXTRA_CONTENT to content,
-                EXTRA_POSITIVE_TEXT to positiveBtnText,
-                EXTRA_NEGATIVE_TEXT to negativeBtnText
+                ImageManageBottomSheetViewModel.KEY_CONTENT to content,
+                ImageManageBottomSheetViewModel.KEY_POSITIVE_TEXT to positiveBtnText,
+                ImageManageBottomSheetViewModel.KEY_NEGATIVE_TEXT to negativeBtnText
             )
-            setPositiveBtnListener(positiveListener)
-            setNegativeBtnListener(negativeListener)
         }
     }
 
     private var _binding: DialogImageManageBinding? = null
     private val binding get() = _binding ?: error("Binding not Initialized")
     private val viewModel: ImageManageBottomSheetViewModel by viewModels()
-
-
-    private var cachePositiveListener: (() -> Unit)? = null
-    private fun setPositiveBtnListener(positiveListener: (() -> Unit)? = null) {
-        cachePositiveListener = positiveListener
-    }
-
-    private var cacheNegativeListener: (() -> Unit)? = null
-    private fun setNegativeBtnListener(negativeListener: (() -> Unit)? = null) {
-        cacheNegativeListener = negativeListener
-    }
-
-    private val defaultBtnText: String by lazy { requireContext().getString(R.string.confirm) }
 
     fun show(fragmentManager: FragmentManager){
         if(fragmentManager.findFragmentByTag(TAG) == null)
@@ -74,19 +56,10 @@ class ImageManageBottomSheetDialog : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Timber.d("onCreateView at bottom sheet")
+        Timber.d("onCreateView at bottom sheet => ${parentFragment is ImageManageBottomSheetEventReceiver}")
         _binding = DialogImageManageBinding.inflate(LayoutInflater.from(context), container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
-        arguments?.let {
-            viewModel.initData(
-                it.getString(EXTRA_CONTENT),
-                it.getString(EXTRA_POSITIVE_TEXT) ?: defaultBtnText,
-                it.getString(EXTRA_NEGATIVE_TEXT),
-                cachePositiveListener,
-                cacheNegativeListener
-            )
-        }
         return binding.root
     }
 
@@ -106,6 +79,12 @@ class ImageManageBottomSheetDialog : BottomSheetDialogFragment() {
         viewModel.uiEvent.observe(viewLifecycleOwner){ event ->
             event.getContentIfNotHandled()?.let {
                 when (it) {
+                    is ImageManageBottomSheetViewModel.UiEvent.PositiveEvent -> {
+                        (parentFragment as? ImageManageBottomSheetEventReceiver)?.onPositiveEventReceive()
+                    }
+                    is ImageManageBottomSheetViewModel.UiEvent.NegativeEvent -> {
+                        (parentFragment as? ImageManageBottomSheetEventReceiver)?.onNegativeEventReceive()
+                    }
                     is ImageManageBottomSheetViewModel.UiEvent.Dismiss -> {
                         this.dismiss()
                     }
