ac853ab092975d0ffa10154a8465868b9332d88b
wjchoi96 <wjchoi96@naver.com>
2022-11-02 22:35:07 +0900

    [Feature]#48 ImageManageBottomSheetViewModel에 SavedStateHandle을 적용하여 상태 보존 수행
     - bundle을 통해 전달받는 state 값들을 SavedStateHandle을 통해 viewModel에서 get하도록 수정
     - Event처리 방식 변경. UiEvent를 통해 Ui로 Event가 발생했으니, 적절한 Callback을 호출하도록 통지

--- app/src/main/java/com/gallery/kakaogallery/presentation/viewmodel/ImageManageBottomSheetViewModel.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/viewmodel/ImageManageBottomSheetViewModel.kt
@@ -2,10 +2,22 @@ package com.gallery.kakaogallery.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
+import com.gallery.kakaogallery.presentation.application.StringResourceProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ImageManageBottomSheetViewModel : ViewModel() {
+@HiltViewModel
+class ImageManageBottomSheetViewModel @Inject constructor(
+    handle: SavedStateHandle,
+    stringResourceProvider: StringResourceProvider
+) : ViewModel() {
+    companion object {
+        const val KEY_CONTENT = "key_content"
+        const val KEY_POSITIVE_TEXT = "key_positive_text"
+        const val KEY_NEGATIVE_TEXT = "key_negative_text"
+    }
     private val _content = MutableLiveData<String>()
     val content: LiveData<String> = _content
 
@@ -14,7 +26,7 @@ class ImageManageBottomSheetViewModel : ViewModel() {
     private val _negativeBtnText = MutableLiveData<String>()
     val negativeBtnText: LiveData<String> = _negativeBtnText
 
-    private val _positiveBtnVisible = MutableLiveData(false)
+    private val _positiveBtnVisible = MutableLiveData(true)
     val positiveBtnVisible: LiveData<Boolean> = _positiveBtnVisible
     private val _negativeBtnVisible = MutableLiveData(false)
     val negativeBtnVisible: LiveData<Boolean> = _negativeBtnVisible
@@ -22,44 +34,34 @@ class ImageManageBottomSheetViewModel : ViewModel() {
     private val _uiEvent = MutableLiveData<SingleEvent<UiEvent>>()
     val uiEvent: LiveData<SingleEvent<UiEvent>> = _uiEvent
 
-    private var positiveListener: (() -> Unit)? = null
-    private var negativeListener: (() -> Unit)? = null
-
-    fun initData(
-        message: String?,
-        positiveBtnText: String,
-        negativeBtnText: String?,
-        positiveListener: (() -> Unit)? = null,
-        negativeListener: (() -> Unit)? = null
-    ){
-        Timber.d("initData at bottom sheet => $message")
-        if(message.isNullOrBlank()){
-            _uiEvent.value = SingleEvent(UiEvent.Dismiss)
-            return
+    init {
+        for(key in handle.keys()){
+            when (key) {
+                KEY_CONTENT -> _content.value = handle[key]
+                KEY_POSITIVE_TEXT -> _positiveBtnText.value =
+                    handle[key] ?: stringResourceProvider.getString(StringResourceProvider.StringResourceId.Confirm)
+                KEY_NEGATIVE_TEXT -> _negativeBtnText.value = handle[key]
+            }
         }
-        message.let { _content.value = it }
-
-        _positiveBtnText.value = positiveBtnText
-        _positiveBtnVisible.value = true
-
-        negativeBtnText?.let { _negativeBtnText.value = it }
-        _negativeBtnVisible.value = negativeBtnText != null
-
-        positiveListener?.let { this.positiveListener = it }
-        negativeListener?.let { this.negativeListener = it }
+        if(_content.value.isNullOrBlank()){
+            _uiEvent.value = SingleEvent(UiEvent.Dismiss)
+        }
+        _negativeBtnVisible.value = _negativeBtnText.value != null
     }
 
     fun positiveEvent(){
-        positiveListener?.invoke()
+        _uiEvent.value = SingleEvent(UiEvent.PositiveEvent)
         _uiEvent.value = SingleEvent(UiEvent.Dismiss)
     }
 
     fun negativeEvent(){
-        negativeListener?.invoke()
+        _uiEvent.value = SingleEvent(UiEvent.NegativeEvent)
         _uiEvent.value = SingleEvent(UiEvent.Dismiss)
     }
 
     sealed class UiEvent {
+        object PositiveEvent: UiEvent()
+        object NegativeEvent: UiEvent()
         object Dismiss: UiEvent()
     }
 }
