ca1fa62df79b222f2c090dcd332206089777a462
wjchoi96 <wjchoi96@naver.com>
2022-11-04 00:42:19 +0900

    [Test]#57 ImageRepositoryImpl 예외 발생 테스트 케이스 추가

--- app/src/test/java/com/gallery/kakaogallery/data/repository/ImageRepositoryImplTest.kt
+++ app/src/test/java/com/gallery/kakaogallery/data/repository/ImageRepositoryImplTest.kt
@@ -4,6 +4,7 @@ import com.gallery.kakaogallery.data.datasource.*
 import com.gallery.kakaogallery.data.entity.remote.response.ImageSearchResponse
 import com.gallery.kakaogallery.data.entity.remote.response.VideoSearchResponse
 import com.gallery.kakaogallery.domain.model.ImageModel
+import com.gallery.kakaogallery.domain.model.MaxPageException
 import com.gallery.kakaogallery.domain.repository.ImageRepository
 import com.google.gson.Gson
 import io.mockk.every
@@ -11,6 +12,7 @@ import io.mockk.mockk
 import io.mockk.verify
 import io.reactivex.rxjava3.core.Single
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Before
 import org.junit.Test
 import java.io.File
@@ -46,6 +48,41 @@ class ImageRepositoryImplTest {
         repository.fetchQueryData(query, page)
         verify { imageSearchDataSource.fetchImageQueryRes(query, page) }
         verify { videoSearchDataSource.fetchVideoQueryRes(query, page) }
+    }
+
+    //state test
+    @Test
+    fun `fetchQueryData는 DataSource하나만 Exception을 발생시키면 정상 동작한다`() {
+        val (query, page) = "test" to 0
+        val actualImageSearchResponse = Gson().fromJson(readResponse("image_search_success.json"), ImageSearchResponse::class.java).documents
+        val actualVideoSearchResponse = Gson().fromJson(readResponse("video_search_success.json"), VideoSearchResponse::class.java).documents
+
+        every { imageSearchDataSource.fetchImageQueryRes(query, page) } returns Single.error(MaxPageException())
+        every { videoSearchDataSource.fetchVideoQueryRes(query, page) } returns Single.just(actualVideoSearchResponse)
+
+        assertThat(repository.fetchQueryData(query, page)
+            .blockingGet().size)
+            .isEqualTo(actualVideoSearchResponse.size)
+
+        every { imageSearchDataSource.fetchImageQueryRes(query, page) } returns Single.just(actualImageSearchResponse)
+        every { videoSearchDataSource.fetchVideoQueryRes(query, page) } returns Single.error(MaxPageException())
+
+        assertThat(repository.fetchQueryData(query, page)
+            .blockingGet().size)
+            .isEqualTo(actualImageSearchResponse.size)
+    }
+
+    //state test
+    @Test
+    fun `fetchQueryData는 DataSource모두 Exception을 발생시키면 해당 Exception을 발생시킨다`() {
+        val (query, page) = "test" to 0
+        every { imageSearchDataSource.fetchImageQueryRes(query, page) } returns Single.error(MaxPageException())
+        every { videoSearchDataSource.fetchVideoQueryRes(query, page) } returns Single.error(MaxPageException())
+
+        val except = MaxPageException().message
+        assertThatThrownBy { repository.fetchQueryData(query, page).blockingGet() }
+            .isInstanceOf(Throwable::class.java)  // blockingGet 에서 RuntimeException 으로 래핑해서 예외를 전달해줌
+            .hasMessageContaining(except)
     }
 
     //state test
