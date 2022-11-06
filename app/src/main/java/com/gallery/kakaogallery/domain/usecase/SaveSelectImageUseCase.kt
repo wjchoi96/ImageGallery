90e9e5e1ab255395f518996e118d4bdb721abc71
wjchoi96 <wjchoi96@naver.com>
2022-11-06 21:59:27 +0900

    [Refactor]#61 SaveSelectImageUseCase 수정
     - Data Entity와 Domain Model 분리 작업 적용
     - Save작업은 Search화면에서 진행되므로 매개변수를 SearchImageModel로 수정
     - 한번에 저장되는 이미지들은 저장 시간이 같으므로, 일괄적으로 saveDateTiem을 매개변수로 전달

--- app/src/main/java/com/gallery/kakaogallery/domain/usecase/SaveSelectImageUseCase.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/usecase/SaveSelectImageUseCase.kt
@@ -2,6 +2,7 @@ package com.gallery.kakaogallery.domain.usecase
 
 import com.gallery.kakaogallery.domain.model.ImageListTypeModel
 import com.gallery.kakaogallery.domain.model.ImageModel
+import com.gallery.kakaogallery.domain.model.SearchImageModel
 import com.gallery.kakaogallery.domain.repository.ImageRepository
 import com.gallery.kakaogallery.domain.util.GalleryDateConvertUtil
 import io.reactivex.rxjava3.core.Completable
@@ -14,20 +15,17 @@ class SaveSelectImageUseCase(
 ) {
     operator fun invoke(selectImageUrlMap: MutableMap<String, Int>, images: List<ImageListTypeModel>): Single<Boolean>{
         return Completable.defer{
-            val saveImages = mutableListOf<ImageModel>()
+            val saveImages = mutableListOf<SearchImageModel>()
             for(selectIdx in selectImageUrlMap.values){
-                val saveTimeMill = Date().time
                 saveImages.add(
                     (images[selectIdx] as ImageListTypeModel.Image).let {
                         it.copy(image = it.image.copy(
-                            saveTimeMill = saveTimeMill,
-                            saveDateTimeToShow = GalleryDateConvertUtil.convertToPrint(saveTimeMill),
                             isSelect = false
                         ))
                     }.image
                 )
             }
-            imageRepository.saveImages(saveImages)
+            imageRepository.saveImages(saveImages, Date().time)
         }.subscribeOn(Schedulers.computation())
             .toSingle {
                 true
