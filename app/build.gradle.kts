92d0a961f98c9cedcf382a7ba7f940ea3bf17dae
wjchoi96 <wjchoi96@naver.com>
2022-11-12 23:51:21 +0900

    [Refactor]#71 buildSrc를 사용하여 버전 관리 수행

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,13 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.gallery.kakaogallery"
-        minSdk = 23
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.1"
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
     }
 
     buildTypes {
@@ -53,63 +53,33 @@ android {
             }
         }
     }
+
     namespace = "com.gallery.kakaogallery"
 
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(Dependencies.Libraries.AndroidX)
 
-    //https://github.com/ReactiveX/RxKotlin
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
-    //https://github.com/Reactivex/Rxandroid/wiki
-    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation(Dependencies.Libraries.Ktx)
 
-    //retrofit2 and rx adapter
-    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    // http log use at retrofit2
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation(Dependencies.Libraries.Rx)
 
-    // https://developer.android.com/jetpack/androidx/releases/activity?hl=ko
-    implementation("androidx.activity:activity-ktx:1.7.0-alpha02")
-    // https://developer.android.com/jetpack/androidx/releases/fragment?hl=ko
-    implementation("androidx.fragment:fragment-ktx:1.6.0-alpha03")
+    implementation(Dependencies.Libraries.Retrofit)
 
-    //swipe refresh layout
-    // https://developer.android.com/jetpack/androidx/releases/swiperefreshlayout?hl=ko
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation(Dependencies.Libraries.Okhttp)
 
-    // glide image
-    // https://velog.io/@rjsdnqkr1/Glide-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-yuk1fmwzo1
-    // https://github.com/bumptech/glide
-    implementation("com.github.bumptech.glide:glide:4.9.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.9.0")
+    implementation(Dependencies.Libraries.Glide)
+    annotationProcessor(Dependencies.Libraries.Glide)
 
-    // dagger-hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-compiler:2.44")
+    implementation(Dependencies.Libraries.Hilt)
+    kapt(Dependencies.Libraries.Hilt)
 
-    // Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    Dependencies.Libraries.Firebase.implementation(this)
 
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation(Dependencies.Libraries.Timber)
 
-    //firebase
-    implementation(platform("com.google.firebase:firebase-bom:31.0.2"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    testImplementation(Dependencies.Libraries.Test)
 
-    testImplementation("org.assertj:assertj-core:3.20.2")
-    androidTestImplementation("org.assertj:assertj-core:3.20.2")
-    testImplementation("io.mockk:mockk:1.13.2")
-    androidTestImplementation("io.mockk:mockk-android:1.13.2")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.7.2")
+    androidTestImplementation(Dependencies.Libraries.AndroidTest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
-        classpath("com.google.gms:google-services:4.3.14")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
+        classPath(Dependencies.Libraries.GradlePlugIn)
+        classPath(Dependencies.Libraries.Hilt)
+        classPath(Dependencies.Libraries.Firebase)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
