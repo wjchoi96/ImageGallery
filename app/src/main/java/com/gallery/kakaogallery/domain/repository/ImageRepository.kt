effdb830e3e739483d53597fba9a17f2b5ec0a4e
wjchoi96 <wjchoi96@naver.com>
2022-12-14 22:54:17 +0900

    [Refactor]#122 ImageRepository.searchQuery메소드의 리턴 형식을 Single<T>에서 Flow<T>로 수정. 중단함수로 설정

--- app/src/main/java/com/gallery/kakaogallery/domain/repository/ImageRepository.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/repository/ImageRepository.kt
@@ -1,14 +1,13 @@
 package com.gallery.kakaogallery.domain.repository
 
 import com.gallery.kakaogallery.domain.model.GalleryImageModel
-import com.gallery.kakaogallery.domain.model.ImageModel
 import com.gallery.kakaogallery.domain.model.SearchImageModel
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
-    fun fetchQueryData(query: String, page: Int): Single<List<SearchImageModel>>
+    suspend fun fetchQueryData(query: String, page: Int): Flow<List<SearchImageModel>>
 
     fun fetchSaveImages(): Observable<List<GalleryImageModel>>
 
