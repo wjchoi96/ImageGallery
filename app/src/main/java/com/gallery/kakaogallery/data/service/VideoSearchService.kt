65b93194db54c4300064bb2cb4c43a4b53b6fb8f
wjchoi96 <wjchoi96@naver.com>
2022-12-14 22:52:26 +0900

    [Modify]#122 VideoSearchSErvice 비동기 처리 수정
     - RxAdapter를 사용하여 비동기 처리 및 Single타입 리턴형식을, 중단함수로 설정하여 비동기 처리를 진행하며 VideoSearchResponse를 리턴하도록 수정

--- app/src/main/java/com/gallery/kakaogallery/data/service/VideoSearchService.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/service/VideoSearchService.kt
@@ -2,16 +2,15 @@ package com.gallery.kakaogallery.data.service
 
 import com.gallery.kakaogallery.data.constant.ApiAddressConstant
 import com.gallery.kakaogallery.data.entity.remote.response.VideoSearchResponse
-import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface VideoSearchService {
     @GET(ApiAddressConstant.Video.VideoSearch)
-    fun requestSearchVideo(
+    suspend fun requestSearchVideo(
         @Query("query") query: String,
         @Query("sort") sort: String,
         @Query("page") page: Int, // 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
         @Query("size") pageSize: Int // 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
-    ): Single<VideoSearchResponse>
+    ): VideoSearchResponse
 }
