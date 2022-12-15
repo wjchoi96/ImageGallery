fd4447d60188ca6cd49158f6b60fa5424476b738
wjchoi96 <wjchoi96@naver.com>
2022-12-15 21:08:02 +0900

    [Refactor]#125 SaveImageDataSource 리턴값을 Rx에서 Flow로 변경, 메소드를 중단함수로 설정

--- app/src/main/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImpl.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImpl.kt
@@ -2,20 +2,21 @@ package com.gallery.kakaogallery.data.datasource
 
 import com.gallery.kakaogallery.data.dao.SaveImageDao
 import com.gallery.kakaogallery.data.entity.local.ImageEntity
-import com.gallery.kakaogallery.domain.model.ImageModel
 import com.gallery.kakaogallery.domain.model.SearchImageModel
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class SaveImageDataSourceImpl @Inject constructor(
     private val saveImageDao: SaveImageDao
 ) : SaveImageDataSource {
 
-    override fun fetchSaveImages(): Observable<List<ImageEntity>> {
+    override suspend fun fetchSaveImages(): Flow<List<ImageEntity>> {
         return saveImageDao.fetchSaveImages()
-            .subscribeOn(Schedulers.io())
+            .flowOn(Dispatchers.IO)
     }
 
     override fun removeImages(idxList: List<Int>): Completable {
