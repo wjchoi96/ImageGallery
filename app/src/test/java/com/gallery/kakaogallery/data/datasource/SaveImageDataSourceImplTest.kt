f0d61f8d0a1d6949bea490521d71aad1c1396f0a
wjchoi96 <wjchoi96@naver.com>
2022-12-15 22:06:20 +0900

    [Test]#125 SaveImageDataSource 테스트 케이스 수정
     - 리턴 형식이 Rx에서 Flow로 바뀐 변경사항 적용

--- app/src/test/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImplTest.kt
+++ app/src/test/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImplTest.kt
@@ -1,14 +1,16 @@
 package com.gallery.kakaogallery.data.datasource
 
 import com.gallery.kakaogallery.data.dao.SaveImageDao
-import com.gallery.kakaogallery.data.entity.local.ImageEntity
-import com.gallery.kakaogallery.domain.model.ImageModel
 import com.gallery.kakaogallery.domain.model.SearchImageModel
+import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import java.util.*
 
+@ExperimentalCoroutinesApi
 @Suppress("NonAsciiCharacters")
 internal class SaveImageDataSourceImplTest {
 
@@ -16,12 +18,12 @@ internal class SaveImageDataSourceImplTest {
 
     //behavior test
     @Test
-    fun `fetchSaveImages는 SaveImageDao의 fetchSaveImages를 호출한다`() {
+    fun `fetchSaveImages는 SaveImageDao의 fetchSaveImages를 호출한다`() = runTest {
         val saveImageDao: SaveImageDao = mockk(relaxed = true)
         saveImageDataSource = SaveImageDataSourceImpl(saveImageDao)
         saveImageDataSource.fetchSaveImages()
 
-        verify { saveImageDao.fetchSaveImages() }
+        coVerify { saveImageDao.fetchSaveImages() }
     }
 
     //behavior test
