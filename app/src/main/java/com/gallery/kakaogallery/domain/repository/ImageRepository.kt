a2efb359b6cf22c741876f3dfab6d4c2d868f8e9
wjchoi96 <wjchoi96@naver.com>
2022-12-15 21:08:18 +0900

    [Refactor]#125 ImageRepository fetchSaveImages메소드의 리턴값을 Rx에서 Flow로 변경, 메소드를 중단함수로 설정

--- app/src/main/java/com/gallery/kakaogallery/domain/repository/ImageRepository.kt
+++ app/src/main/java/com/gallery/kakaogallery/domain/repository/ImageRepository.kt
@@ -3,13 +3,12 @@ package com.gallery.kakaogallery.domain.repository
 import com.gallery.kakaogallery.domain.model.GalleryImageModel
 import com.gallery.kakaogallery.domain.model.SearchImageModel
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
     suspend fun fetchQueryData(query: String, page: Int): Flow<List<SearchImageModel>>
 
-    fun fetchSaveImages(): Observable<List<GalleryImageModel>>
+    suspend fun fetchSaveImages(): Flow<List<GalleryImageModel>>
 
     fun removeImages(idxList: List<Int>): Completable
 
