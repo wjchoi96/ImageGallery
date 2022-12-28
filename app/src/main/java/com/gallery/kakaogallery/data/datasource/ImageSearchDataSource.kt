2fbf9ba0e1152b762159d9c410e30dd727ef52c6
wjchoi96 <wjchoi96@naver.com>
2022-12-28 18:37:56 +0900

    [Refactor]#143 ImageSearchDataSource 의 suspend 함수 해제

--- app/src/main/java/com/gallery/kakaogallery/data/datasource/ImageSearchDataSource.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/ImageSearchDataSource.kt
@@ -4,5 +4,5 @@ import com.gallery.kakaogallery.data.entity.remote.response.ImageSearchResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ImageSearchDataSource {
-    suspend fun fetchImageQueryRes(query: String, page: Int): Flow<List<ImageSearchResponse.Document>>
+    fun fetchImageQueryRes(query: String, page: Int): Flow<List<ImageSearchResponse.Document>>
 }
