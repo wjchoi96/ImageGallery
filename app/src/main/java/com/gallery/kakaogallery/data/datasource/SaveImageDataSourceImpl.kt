12e858091b0b5e63e96b94dcc6dd5d9f987a3c5a
wjchoi96 <wjchoi96@naver.com>
2023-01-11 16:22:40 +0900

    [Refactor]#136 SaveImageDataSourceImpl가 Dispatcher를 주입받아 사용하도록 변경

--- app/src/main/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImpl.kt
+++ app/src/main/java/com/gallery/kakaogallery/data/datasource/SaveImageDataSourceImpl.kt
@@ -3,25 +3,27 @@ package com.gallery.kakaogallery.data.datasource
 import com.gallery.kakaogallery.data.dao.SaveImageDao
 import com.gallery.kakaogallery.data.entity.local.ImageEntity
 import com.gallery.kakaogallery.domain.model.SearchImageModel
-import kotlinx.coroutines.Dispatchers
+import com.gallery.kakaogallery.presentation.di.IODispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class SaveImageDataSourceImpl @Inject constructor(
-    private val saveImageDao: SaveImageDao
+    private val saveImageDao: SaveImageDao,
+    @IODispatcher private val dispatcher: CoroutineDispatcher
 ) : SaveImageDataSource {
 
     override fun fetchSaveImages(): Flow<List<ImageEntity>> {
         return saveImageDao.fetchSaveImages()
-            .flowOn(Dispatchers.IO)
+            .flowOn(dispatcher)
     }
 
     override fun removeImages(idxList: List<Int>): Flow<Boolean> = flow {
         saveImageDao.removeImages(idxList)
         emit(true)
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 
     override fun saveImages(
         image: List<SearchImageModel>,
@@ -29,5 +31,5 @@ class SaveImageDataSourceImpl @Inject constructor(
     ): Flow<Boolean> = flow {
         saveImageDao.saveImages(image, saveDateTimeMill)
         emit(true)
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 }
