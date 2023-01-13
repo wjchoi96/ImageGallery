a45b9a3adce01b918a8546309d3bb199ba3e6dfd
wjchoi96 <wjchoi96@naver.com>
2023-01-13 16:53:38 +0900

    [Feature]#145 View를 벗어나서 ActionUp을 하거나 Cancel되는 경우 View의 Scale을 원상복구하는 코드 추가

--- app/src/main/java/com/gallery/kakaogallery/presentation/ui/searchimage/GalleryImageItemViewHolder.kt
+++ app/src/main/java/com/gallery/kakaogallery/presentation/ui/searchimage/GalleryImageItemViewHolder.kt
@@ -39,11 +39,26 @@ class GalleryImageItemViewHolder private constructor(
     }
 
     init {
-        binding.background.setOnTouchListener { _, motionEvent ->
+        binding.background.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     Timber.d("animation debug => ACTION_DOWN")
                     scaleAnimate(binding.cvImage, 0.93f)
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    Timber.d("animation debug => ACTION_UP")
+                    when (motionEvent.x !in 0f..view.width.toFloat() || motionEvent.y !in 0f..view.height.toFloat()) {
+                        true -> {
+                            scaleAnimate(binding.cvImage, 1f)
+                        }
+                        else -> {}
+                    }
+                }
+
+                MotionEvent.ACTION_CANCEL -> {
+                    Timber.d("animation debug => ACTION_CANCEL")
+                    scaleAnimate(binding.cvImage, 1f)
                 }
             }
             false
